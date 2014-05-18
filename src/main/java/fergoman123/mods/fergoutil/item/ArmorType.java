49868dbbdaa22c2500ee7b8d95a5e1daa97a4e59
Michael Ferguson <mickferg1@gmail.com>
2014-05-18 18:52:14 +1000

    Cleanup

---
+++ src/main/java/fergoman123/mods/fergoutil/item/ArmorType.java
@@ -0,0 +1,11 @@
+package fergoman123.mods.fergoutil.item;
+
+/**
+ * Created by Fergoman123 on 17/05/2014.
+ */
+public enum ArmorType {
+
+    HELMET, CHEST, LEGS, BOOTS;
+
+    public static final ArmorType[] types = ArmorType.values();
+}
