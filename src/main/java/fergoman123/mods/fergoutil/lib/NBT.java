c50f3280fe2586dae2e53c089be243a5fb45fb6f
Michael Ferguson <mickferg1@gmail.com>
2014-06-15 19:03:09 +1000

    Auto Commit - Sun Jun 15 19:02:56 AUSEST 2014 (Fergoman007-PC)

---
+++ src/main/java/fergoman123/mods/fergoutil/lib/NBT.java
@@ -0,0 +1,7 @@
+package fergoman123.mods.fergoutil.lib;
+
+public class NBT {
+
+    public static final String UUID_MOST_SIG = "UUIDMostSig";
+    public static final String UUID_LEAST_SIG = "UUIDLeastSig";
+}
