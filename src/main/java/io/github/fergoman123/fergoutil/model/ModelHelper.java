064f017bbd199a2039d59aab9016e1aee200e9b9
Fergoman123 <mickferg1@gmail.com>
2016-02-15 15:36:42 +1100

    made getModelMesher private

--- src/main/java/io/github/fergoman123/fergoutil/model/ModelHelper.java
+++ src/main/java/io/github/fergoman123/fergoutil/model/ModelHelper.java
@@ -1,7 +1,6 @@
 package io.github.fergoman123.fergoutil.model;
 
-import io.github.fergoman123.fergoutil.item.ItemFergo;
-import io.github.fergoman123.fergoutil.item.ItemMultiFergo;
+import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ItemModelMesher;
 import net.minecraft.client.resources.model.ModelBakery;
@@ -10,32 +9,31 @@ import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
 
 public class ModelHelper {
-	public static ItemModelMesher getModelMesher(){
-		return Minecraft.getMinecraft().getRenderItem().getItemModelMesher();
-	}
-	
-	public static void register(ModelEntry model){
-		getModelMesher().register(model.item, model.meta, new ModelResourceLocation(model.modid + ":" + model.name, "inventory"));
-	}
-	
-	public static void addVariant(ModelVariant variant){
-		ModelBakery.registerItemVariants(variant.item, new ResourceLocation(variant.modid + ":" + variant.name));
-	}
+    private static ItemModelMesher getModelMesher() {
+        return Minecraft.getMinecraft().getRenderItem().getItemModelMesher();
+    }
 
-	public static void registerModel(Item item){
-		if (item instanceof ItemFergo){
-			ItemFergo itemFergo = (ItemFergo)item;
-			getModelMesher().register(itemFergo, 0, new ModelResourceLocation(itemFergo.getRegistryName(), "inventory"));
-			ModelBakery.registerItemVariants(item, new ResourceLocation(item.getRegistryName()));
-		} else if (item instanceof ItemMultiFergo){
-			ItemMultiFergo multiFergo = (ItemMultiFergo)item;
-			for (int i = 0; i < multiFergo.getSubNames().length; i++) {
-				getModelMesher().register(multiFergo, i, new ModelResourceLocation(multiFergo.getMod() + ":" + multiFergo.getSubNames()[i], "inventory"));
-				ModelBakery.registerItemVariants(multiFergo, new ResourceLocation(multiFergo.getMod() + ":" + multiFergo.getSubNames()[i]));
-			}
-		} else {
-			getModelMesher().register(item, 0, new ModelResourceLocation(item.getRegistryName(), "inventory"));
-			ModelBakery.registerItemVariants(item, new ResourceLocation(item.getRegistryName()));
-		}
-	}
+    public static void register(Item item, int meta, ModelResourceLocation model) {
+        getModelMesher().register(item, meta, model);
+    }
+
+    public static void register(Item item, ModelResourceLocation model) {
+        register(item, 0, model);
+    }
+
+    public static void register(Block block, int meta, ModelResourceLocation model) {
+        register(Item.getItemFromBlock(block), meta, model);
+    }
+
+    public static void register(Block block, ModelResourceLocation model) {
+        register(block, 0, model);
+    }
+
+    public static void variant(Item item, ResourceLocation variants) {
+        ModelBakery.registerItemVariants(item, variants);
+    }
+
+    public static void variant(Block block, ResourceLocation variant) {
+        variant(Item.getItemFromBlock(block), variant);
+    }
 }
